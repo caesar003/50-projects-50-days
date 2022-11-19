ee6ceb6c5d990c8786a7fd0a9fd9afa77e8b1719
caesar003 <caesarmuksid@gmail.com>
2022-11-19 22:15:55 +0700

    added movie app

---
+++ 18 Day 17 - Movie App/script.js
@@ -0,0 +1,65 @@
+const API_KEY = "95760ade3522c9ed42682db0af223d8a";
+const BASE_URL = "https://api.themoviedb.org/3/";
+const API_URL = `${BASE_URL}discover/movie?sort_by=popularity.desc&api_key=${API_KEY}&page=1`;
+const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
+const SEARCH_API = `${BASE_URL}search/movie?api_key=${API_KEY}&query=`;
+
+const main = document.getElementById("main");
+const form = document.getElementById("form");
+const search = document.getElementById("search");
+
+getMovies(API_URL);
+
+async function getMovies(url) {
+    const res = await fetch(url);
+    const data = await res.json();
+
+    showMovies(data.results);
+}
+
+function showMovies(movies) {
+    main.innerHTML = "";
+    movies.forEach((movie) => {
+        const { title, poster_path, vote_average, overview, release_date } =
+            movie;
+
+        const movieYear = new Date(release_date).getFullYear();
+        const movieEl = document.createElement("div");
+        movieEl.classList.add("movie");
+        movieEl.innerHTML = `
+            <img src="${IMG_PATH + poster_path}" alt="${title}" />
+            <div class="movie-info">
+                <h3>${title} (${movieYear})</h3>
+                <span class="${getClassByRate(
+                    vote_average
+                )}">${vote_average}</span>
+            </div>
+            <div class="overview">
+                <h3>Overview</h3>
+                ${overview} 
+            </div> `;
+        main.appendChild(movieEl);
+    });
+}
+
+function getClassByRate(vote) {
+    if (vote >= 8) {
+        return "green";
+    } else if (vote >= 5) {
+        return "orange";
+    } else {
+        return "red";
+    }
+}
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const searchTerm = search.value;
+
+    if (searchTerm && searchTerm !== "") {
+        getMovies(SEARCH_API + searchTerm);
+        search.value = "";
+    } else {
+        window.location.reload();
+    }
+});
